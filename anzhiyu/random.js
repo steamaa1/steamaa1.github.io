1e62223520f89c9e162966d6516306f2f0455be3
steamaa1 <qw3580305140@outlook.com>
2025-09-06 13:46:06 +0800

    Site updated: 2025-09-06 13:46:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2025/08/09/hello-world/","2025/08/22/关于那些我创作的歌词/"];function toRandomPost(){
+var posts=["2025/08/09/hello-world/","2025/08/22/关于那些我创作的歌词/","2025/09/06/上学两三事/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
